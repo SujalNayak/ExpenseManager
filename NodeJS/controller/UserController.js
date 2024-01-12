0d0c07f117d8ad53034093494118fa559513865e
SujalNayak <121352118+SujalNayak@users.noreply.github.com>
2024-01-12 22:26:09 +0530

    jan12

--- NodeJS/controller/UserController.js
+++ NodeJS/controller/UserController.js
@@ -1,4 +1,12 @@
 const userSchema = require("../model/UserModel");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  });
+  
 
 const getUsers = (req,res) => {
 
@@ -15,27 +23,55 @@ const getUsers = (req,res) => {
             error:err
         })
     });
-
+    
 }
 
+const upload = multer({ storage: storage }).single("email");
 const addUser = (req,res) => {
     const user = new userSchema(req.body);
     //save user data
-    user.save().then((data) => {
-        res.status(201).json({
-            message: "Data has been saved",
-            success: true,
-            data: data
-        })
-    }).catch((err) => {
-        res.status(500).json({
-            message: "error",
-            error: err
-        })
-        console.log(err);   
-    })
-}
-
+    // user.save().then((data) => {
+    //     res.status(201).json({
+    //         message: "Data has been saved",
+    //         success: true,
+    //         data: data
+    //     })
+    // }).catch((err) => {
+    //     res.status(500).json({
+    //         message: "error",
+    //         error: err
+    //     })
+    //     console.log(err);   
+    // })
+    upload(req, res, async function (err) {
+        if (err) {
+            res.json({ message: err });
+        } 
+        else {
+            const userObj = {
+                id: req.body.id,
+                fname: req.body.fname,            
+                lname: req.body.lname,
+                email: req.body.email,
+                password: req.body.password,
+                mobile_no: req.body.mobile_no,
+                status: req.body.status
+             };
+      
+            const storedUser = await user.save(userObj);
+            if (storedUser) {
+              res.status(200).json({
+                message: "User added successfully...",
+                data: storedBook,
+              });
+            } else {
+              res.status(500).json({
+                message: "Error while adding user...",
+              });
+            }
+        }
+    });
+};
 // const updateUser = (req,res) => {
 
 //     const id = parseInt(req.params.id);
