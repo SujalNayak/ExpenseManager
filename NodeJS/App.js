51ee1697ff81222b6c16036bad69b03849200f0b
Sujal Nayak <121352118+SujalNayak@users.noreply.github.com>
2024-01-29 10:32:31 +0530

    Update App.js

--- NodeJS/App.js
+++ NodeJS/App.js
@@ -3,7 +3,12 @@ const mongoose= require("mongoose");
 const cors = require("cors");
 const app = express();
 const PORT = 3002;
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'https://stock-market-prediction-theta.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 app.use(express.json());
 
 const userRoutes = require("./routes/UserRoutes");
